af7d3ffa551542cbf1bf1939a52260ebaa914f91
ppabam <tom@diginori.com>
2025-01-14 17:19:43 +0900

    first test OK

--- src/ppabam_check_os_ver/osver.py
+++ src/ppabam_check_os_ver/osver.py
@@ -3,5 +3,7 @@ def get_os_pretty_name() -> str:
         for line in file:
             if line.startswith('PRETTY_NAME'):
                 # Ubuntu 24.04.1 LTS
-                return line.split('=')[1].replace('\n','').strip("\"")
+                r = line.split('=')[1].replace('\n','').strip("\"")
+                print(r)
+                return r
     return None
---
+++ tests/test_first.py
@@ -0,0 +1,5 @@
+from ppabam_check_os_ver.osver import get_os_pretty_name
+
+def test_first():
+    v = get_os_pretty_name()
+    assert v == "Ubuntu 24.04.1 LTS"
